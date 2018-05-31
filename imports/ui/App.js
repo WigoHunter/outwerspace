f8a1127ebe433e894d174131ff61b310ac30d87e
Kevin Hsu <mattandkevin1060@gmail.com>
2018-05-31 22:33:25 +0800

    map setup complete

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import MapComponent from './MapComponent';
 
 class App extends React.Component {
     render() {
         return (
-            <div className="map">
+            <div>
+                <div style={{
+                    position: "absolute",
+                    height: "300px",
+                    width: "200px",
+                    background: "blue",
+                    zIndex: 10000,
+                    opacity: 0.7
+                }} />
+                <MapComponent />
             </div>
         )
     }
